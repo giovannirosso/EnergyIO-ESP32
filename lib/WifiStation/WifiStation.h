2a93c307db8ccd59599c6ade8d64cfb1b6b8ccef
Mateus Mello <mateus.mello.oliva@gmail.com>
2021-03-29 22:42:08 -0300

    added wifi station lib

---
+++ lib/WifiStation/WifiStation.h
@@ -0,0 +1,50 @@
+#ifndef WIFI_STATION_H
+#define WIFI_STATION_H
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+typedef enum
+{
+    CONNECTED,
+    NOT_CONNECTED,
+    UNKNOWN
+} WifiState;
+
+typedef struct
+{
+    int maxRetries;
+} EventHandlerArgs;
+
+// FreeRTOS Signal handler
+void eventHandler(void *arg, esp_event_base_t event_base,
+                  int32_t event_id, void *event_data);
+
+/** Inits a Wifi Station 
+ * @param ssid Network's SSID which the esp will connect
+ * @param password Network's password which the esp will connect
+ * @param retries Number of connection retries
+ **/
+void init(char *ssid, char *password, int retries);
+
+/**
+ * Blocks execution until it gets a state response from eventHandler
+ */
+WifiState wifiState();
+
+#endif //WIFI_STATION_H
